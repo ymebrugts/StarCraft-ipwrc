7395ae2f4efcb72d18acc41fd26366865417ee03
Yme <36673314+ymebrugts@users.noreply.github.com>
2018-12-21 13:24:16 +0100

    Add missing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './shop/shop.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './home/header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ShopComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
